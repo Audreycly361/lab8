8711937f977f1dad6d3b029979979d5e17afa419
liuyi Chen <lic115@ucsd.edu>
2018-03-01 00:18:20 -0800

    ai

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,4 +1,4 @@
-// 'use strict';
+//'use strict';
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
@@ -13,14 +13,39 @@ function initializePage() {
  	initCamera();
  	initGestures();
  	initRSVPForm();
+ 	$(".judge-img").click(initGestures);
+
 }
 
 // init jQuery gestures  
 function initGestures() {
-	// add gestures listener here
+
+	$(function(){
+		$("div.box").bind("taphold".tapholdHandler);
+		$(".judge-img").bind("taphold", tapholdHandler);
+
+		function tapholdHandler(event){
+			$(event.target).addClass("taphold");
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix: " + targetIDPrefix);
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+  $('#rsvpForm').submit(function(e){
+ 	 e.preventDefault();
+  	console.log("submitting form...");
+  	var rsvpEmail = $('#rsvpEmail').val();
+  	$.post('addRSVP',{rsvpEmail: rsvpEmail}, postCallback);
+
+  });
+
+  function postCallback(res){
+
+  	alert("RSVP form successfully submitted!");
+  	$('#rsvpEmail').val('');
+  }
 }
